70c9b0a5209d31c0f08a938bb78fa7ab99c1e852
hngoldman995 <hannahgoldman995@gmail.com>
2023-04-10 13:06:26 -0400

    Today's alterations to JS

--- js/app.js
+++ js/app.js
@@ -2,6 +2,49 @@
 
 
 
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '6c049fd770msh1cdc73180460014p113da5jsn145699b64423',
+// 		'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://opencritic-api.p.rapidapi.com/game/upcoming', options)
+// 	.then(response => {
+//         return response.json();
+//     })
+// 	.then(data => {
+//         console.log(data)
+
+//         // data.forEach(game => {
+//         //     const gameTitles = `<ul>${game.name}</ul>`;
+//         //     const gameImages = `<img src="${game.images.banner.og}">`;
+//         //     document.querySelector('section').insertAdjacentHTML('beforeend', gameTitles);
+
+//         //     document.querySelector('ul').insertAdjacentHTML('beforeend', movieImages)
+//         // })   
+//         const gamesContainer = document.createElement('div')
+// data.forEach(game => {
+//     const gameWrapper = document.createElement('div')
+//     const gameTitle = document.createElement('h2')
+//     const gameImg = document.createElement('img')
+//     const gameRelease = document.createElement('h4')
+//     gameRelease.innerText = `${game.firstReleaseDate}`
+//     gameTitle.innerText = `${game.name}`;
+//     gameImg.src = `${game.images.box}"`;
+//     gameWrapper.appendChild(gameTitle)
+//     gameWrapper.appendChild(gameImg)
+//     gameWrapper.appendChild(gameRelease)
+//     gamesContainer.appendChild(gameWrapper)
+// });
+// document.body.appendChild(gamesContainer)
+
+//     })
+// 	.catch(err => console.error(err));
+
+    
+    
 const options = {
 	method: 'GET',
 	headers: {
@@ -10,39 +53,30 @@ const options = {
 	}
 };
 
-fetch('https://opencritic-api.p.rapidapi.com/game/upcoming', options)
-	.then(response => {
-        return response.json();
-    })
+fetch('https://opencritic-api.p.rapidapi.com/game/recently-released', options)
+	.then(response => response.json())
 	.then(data => {
-        console.log(data)
-        
-        // data.forEach(game => {
-        //     const gameTitles = `<ul>${game.name}</ul>`;
-        //     const gameImages = `<img src="${game.images.banner.og}">`;
-        //     document.querySelector('section').insertAdjacentHTML('beforeend', gameTitles);
 
-        //     document.querySelector('ul').insertAdjacentHTML('beforeend', movieImages)
-        // })   
+        console.log(data)
         const gamesContainer = document.createElement('div')
-data.forEach(game => {
-    const gameWrapper = document.createElement('div')
-    const gameTitle = document.createElement('h2')
-    const gameImg = document.createElement('img')
-    const gameRelease = document.createElement('h4')
-    gameRelease.innerText = `${game.firstReleaseDate}`
-    gameTitle.innerText = `${game.name}`;
-    gameImg.src = `${game.images.box}"`;
-    gameWrapper.appendChild(gameTitle)
-    gameWrapper.appendChild(gameImg)
-    gameWrapper.appendChild(gameRelease)
-    gamesContainer.appendChild(gameWrapper)
-});
-document.body.appendChild(gamesContainer)
+
+        data.forEach( game => {
+            const gameWrapper = document.createElement('div');
+            const gameTitle = document.createElement('h2');
+            const gameImg = document.createElement('img');
+            const gameRelease = document.createElement('h4');
+            gameRelease.innerText = `${game.firstReleaseDate}`;
+            gameTitle.innerText = `${game.name}`;
+            gameImg.src = `${game.images.box.og}`;
+            gameWrapper.appendChild(gameTitle);
+            gameWrapper.appendChild(gameImg);
+            gameWrapper.appendChild(gameRelease);
+            gamesContainer.appendChild(gameWrapper);
+        });
+        document.body.appendChild(gamesContainer);
+        
 
     })
-	.catch(err => console.error(err));
+    
 
-    
-    
-    
+	.catch(err => console.error(err));
