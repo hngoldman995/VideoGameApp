781ec3676d531dc4eae853285904f9c942d728af
hngoldman995 <hannahgoldman995@gmail.com>
2023-04-11 15:40:47 -0400

    Today's Alterations

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@
 //     const gameRelease = document.createElement('h4')
 //     gameRelease.innerText = `${game.firstReleaseDate}`
 //     gameTitle.innerText = `${game.name}`;
-//     gameImg.src = `${game.images.box}"`;
+//     gameImg.src = `https://opencritic-api.p.rapidapi.com/game/upcoming/${game.images.box.og}`;
 //     gameWrapper.appendChild(gameTitle)
 //     gameWrapper.appendChild(gameImg)
 //     gameWrapper.appendChild(gameRelease)
@@ -45,32 +45,69 @@
 
     
     
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '6c049fd770msh1cdc73180460014p113da5jsn145699b64423',
-		'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com'
-	}
-};
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '6c049fd770msh1cdc73180460014p113da5jsn145699b64423',
+// 		'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com'
+// 	}
+// };
 
-fetch('https://opencritic-api.p.rapidapi.com/game/recently-released', options)
-	.then(response => response.json())
-	.then(data => {
+// fetch('https://opencritic-api.p.rapidapi.com/game/recently-released', options)
+// 	.then(response => response.json())
+// 	.then(data => {
 
-        console.log(data)
-        const gamesContainer = document.createElement('div')
+//         console.log(data)
+//         const gamesContainer = document.createElement('div')
+
+//         data.forEach( game => {
+//             const gameWrapper = document.createElement('div');
+//             const gameTitle = document.createElement('h2');
+//             const gameImg = document.createElement('img');
+//             const gameRelease = document.createElement('h4');
+//             gameRelease.innerText = `${game.firstReleaseDate}`;
+//             gameTitle.innerText = `${game.name}`;
+//             gameImg.src = `https://opencritic-api.p.rapidapi.com/game/recently-released/${game.images.box.og}`;
+//             gameWrapper.appendChild(gameTitle);
+//             gameWrapper.appendChild(gameImg);
+//             gameWrapper.appendChild(gameRelease);
+//             gamesContainer.appendChild(gameWrapper);
+//         });
+//         document.body.appendChild(gamesContainer);
+//     })
+    
+
+// 	.catch(err => console.error(err));
+
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '6c049fd770msh1cdc73180460014p113da5jsn145699b64423',
+            'X-RapidAPI-Host': 'videogames-news2.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://videogames-news2.p.rapidapi.com/videogames_news/recent', options)
+        .then(response => response.json())
+        .then(data => {
+            
+            console.log(data)
+            const gamesContainer = document.createElement('div')
+            gamesContainer.classList.add("flex-container")
 
         data.forEach( game => {
             const gameWrapper = document.createElement('div');
             const gameTitle = document.createElement('h2');
             const gameImg = document.createElement('img');
-            const gameRelease = document.createElement('h4');
-            gameRelease.innerText = `${game.firstReleaseDate}`;
-            gameTitle.innerText = `${game.name}`;
-            gameImg.src = `https://opencritic-api.p.rapidapi.com/game/upcoming/${game.images.box.og}`;
+            gameImg.classList.add('images')
+            const gameDescription = document.createElement('h4');
+            gameDescription.innerText = `${game.description}`;
+            gameTitle.innerText = `${game.title}`;
+            gameImg.src = `${game.image}`;
             gameWrapper.appendChild(gameTitle);
             gameWrapper.appendChild(gameImg);
-            gameWrapper.appendChild(gameRelease);
+            gameWrapper.appendChild(gameDescription);
             gamesContainer.appendChild(gameWrapper);
         });
         document.body.appendChild(gamesContainer);
@@ -78,5 +115,8 @@ fetch('https://opencritic-api.p.rapidapi.com/game/recently-released', options)
     
 
 	.catch(err => console.error(err));
+
+            
+    
 
     
